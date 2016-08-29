5e3506739b2f02ac533c1e5a48792b9a8ae93d37
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-08-28 18:12:32 -0700

    Initial commit
    Import gtk-js-app and adapt it to edu.stanford.thingengine

---
+++ README.md
@@ -0,0 +1,22 @@
+# ThingEngine
+
+## Help the World Connect the Dots
+
+ThingEngine is the workhorse behind ThingPedia, the open source platform for IoT rules
+that you can execute anywhere you want.
+
+ThingEngine comes in various form:
+
+- As a phone app, for Android
+- As an installable app for a home server
+- As a web service hosted at <https://thingengine.stanford.edu>
+- A GTK+/GNOME app for desktops
+
+This module contains the GNOME version of ThingEngine, and
+depends on a number of other modules.
+
+ThingEngine is part of Open Thing Platform, a research project led by
+prof. Monica Lam, from Stanford University.  You can find more
+information at <http://thingengine.stanford.edu/about>. User
+documentation is available in
+[thingengine-core](https://github.com/Stanford-IoT-Lab/thingengine-core).
---
+++ src/main.js
@@ -0,0 +1,79 @@
+// -*- Mode: js; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*-
+//
+// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See LICENSE for details
+
+pkg.initGettext();
+pkg.initFormat();
+pkg.require({ 'Gdk': '3.0',
+              'Gio': '2.0',
+              'GLib': '2.0',
+              'GObject': '2.0',
+              'Gtk': '3.0' });
+
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Util = imports.util;
+const Window = imports.window;
+
+function initEnvironment() {
+    window.getApp = function() {
+        return Gio.Application.get_default();
+    };
+}
+
+const MyApplication = new Lang.Class({
+    Name: 'MyApplication',
+    Extends: Gtk.Application,
+
+    _init: function() {
+        this.parent({ application_id: pkg.name });
+
+        GLib.set_application_name(_("My JS Application"));
+    },
+
+    _onQuit: function() {
+        this.quit();
+    },
+
+    _initAppMenu: function() {
+        let builder = new Gtk.Builder();
+        builder.add_from_resource('/edu/stanford/thingengine/app-menu.ui');
+
+        let menu = builder.get_object('app-menu');
+        this.set_app_menu(menu);
+    },
+
+    vfunc_startup: function() {
+        this.parent();
+
+        Util.loadStyleSheet('/edu/stanford/thingengine/application.css');
+
+        Util.initActions(this,
+                         [{ name: 'quit',
+                            activate: this._onQuit }]);
+        this._initAppMenu();
+
+        log(_("My JS Application started"));
+    },
+
+    vfunc_activate: function() {
+        (new Window.MainWindow({ application: this })).show();
+    },
+
+    vfunc_shutdown: function() {
+        log(_("My JS Application exiting"));
+
+        this.parent();
+    }
+});
+
+function main(argv) {
+    initEnvironment();
+
+    return (new MyApplication()).run(argv);
+}
