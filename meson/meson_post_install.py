47287736515b2195b3efe6c88f1ece865d64dd43
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-12-29 20:25:29 +0100

    Add a flatpak definition file
    And add the meson build rules to automatically install the nodejs
    service and its dependencies (using yarn)

--- meson/meson_post_install.py
+++ meson/meson_post_install.py
@@ -2,9 +2,23 @@
 
 import os
 import subprocess
+import shutil
 
 schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
 	print('Compiling gsettings schemas...')
 	subprocess.call(['glib-compile-schemas', schemadir])
+
+sourcedir = os.path.join(os.environ['MESON_SOURCE_ROOT'], 'service')
+servicedir = os.path.join(os.environ.get('MESON_DESTDIR_INSTALL_PREFIX', os.environ['MESON_INSTALL_PREFIX']), 'lib', 'edu.stanford.Almond', 'service')
+
+print('Installing node.js service...')
+try:
+    shutil.rmtree(servicedir)
+except FileNotFoundError:
+    pass
+shutil.copytree(sourcedir, servicedir, symlinks=True)
+
+yarn = os.environ.get('YARN', 'yarn')
+subprocess.call([yarn, "install", "--frozen-lockfile"], cwd=servicedir)
