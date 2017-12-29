1205408c1ed33b927889a94dcbab483dfbc13234
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-12-29 21:16:21 +0100

    Fix paths, and run update-icon-theme
    Otherwise the icon cannot be found

--- meson/meson_post_install.py
+++ meson/meson_post_install.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
 import os
-import subprocess
 import shutil
+import pathlib
+import subprocess
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+prefix = pathlib.Path(os.environ['MESON_INSTALL_PREFIX'])
+datadir = prefix / 'share'
+destdir = os.environ.get('DESTDIR', '')
 
-if not os.environ.get('DESTDIR'):
-	print('Compiling gsettings schemas...')
-	subprocess.call(['glib-compile-schemas', schemadir])
+if not destdir:
+    print('Compiling gsettings schemas...')
+    subprocess.call(['glib-compile-schemas', str(datadir / 'glib-2.0' / 'schemas')])
+
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', str(datadir / 'icons' / 'hicolor')])
+
+    print('Updating desktop database...')
+    subprocess.call(['update-desktop-database', '-q', str(datadir / 'applications')])
 
 sourcedir = os.path.join(os.environ['MESON_SOURCE_ROOT'], 'service')
 servicedir = os.path.join(os.environ.get('MESON_DESTDIR_INSTALL_PREFIX', os.environ['MESON_INSTALL_PREFIX']), 'lib', 'edu.stanford.Almond', 'service')
@@ -21,4 +30,6 @@ except FileNotFoundError:
 shutil.copytree(sourcedir, servicedir, symlinks=True)
 
 yarn = os.environ.get('YARN', 'yarn')
-subprocess.call([yarn, "install", "--frozen-lockfile"], cwd=servicedir)
+subprocess.check_call([yarn, "install", "--offline", "--frozen-lockfile"], cwd=servicedir)
+
+shutil.rmtree(os.path.join(servicedir, "deps"))
