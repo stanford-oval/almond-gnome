d15422580c2d618ab1a97d213720e31f6dda3749
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-12-30 00:04:58 +0100

    More fixes
    We're close to making it work now...

--- service/tools/update-flatpak-definition.js
+++ service/tools/update-flatpak-definition.js
@@ -19,25 +19,27 @@ const yarnlock = YarnLock.parse(yarnlockfile);
 
 const urls = new Set;
 for (let name in yarnlock.object) {
-    if (name.startsWith('@yarnpkg/lockfile'))
-        continue;
-
     const url = yarnlock.object[name].resolved;
     urls.add(url);
 }
 for (let url of urls) {
     const parsed = Url.parse(url);
-    const basename = path.basename(parsed.pathname);
+    let basename = path.basename(parsed.pathname);
+
+    if (basename.startsWith('lockfile'))
+        basename = '@yarnpkg-' + basename;
 
     const sha256 = crypto.createHash('sha256');
     sha256.update(fs.readFileSync(path.resolve('deps', basename)));
 
-    lastModule.sources.push({
+    const source = {
         type: 'file',
         url: url,
         sha256: sha256.digest('hex'),
-        dest: 'service/deps'
-    });
+        dest: 'service/deps',
+        'dest-filename': basename
+    };
+    lastModule.sources.push(source);
 }
 
 fs.writeFileSync('../edu.stanford.Almond.json', JSON.stringify(existing, undefined, 2));
