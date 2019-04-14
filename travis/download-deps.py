30cdb8befc94d7627e56767d2ef46f3da5c26016
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-04-13 23:51:02 -0700

    travis: download service/deps manually
    Because we don't call flatpak-builder to do it for us

---
+++ travis/download-deps.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import json
+import os
+import subprocess
+import urllib.request
+import urllib.parse
+import ssl
+import shutil
+
+os.makedirs('./service/deps', exist_ok=True)
+
+with open('./service/flatpak.json') as fp:
+    data = json.load(fp)
+
+ssl_context = ssl.create_default_context()
+for src in data:
+    url = urllib.parse.urlparse(src['url'])
+    dest = os.path.join('./service/deps', src['dest-filename'])
+    print(src['dest-filename'])
+    with urllib.request.urlopen(src['url'], context=ssl_context) as fsrc, \
+        open(dest, 'wb') as fdest:
+        shutil.copyfileobj(fsrc, fdest)
+
