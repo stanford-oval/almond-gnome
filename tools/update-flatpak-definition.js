544eed70b5f8ac486a705ac7fd3fa52395bbf122
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-27 11:30:05 -0700

    update-flatpak: don't rely on the yarn offline mirror
    Otherwise I have to fiddle with yarnrc every time I run this tool

--- tools/update-flatpak-definition.js
+++ tools/update-flatpak-definition.js
@@ -5,39 +5,44 @@ const fs = require('fs');
 const Url = require('url');
 const path = require('path');
 const crypto = require('crypto');
+const Tp = require('thingpedia');
 
-// load the yarn.lock files
-const yarnlockfile = fs.readFileSync('./yarn.lock').toString();
-const yarnlock = YarnLock.parse(yarnlockfile);
+async function main() {
+    // load the yarn.lock files
+    const yarnlockfile = fs.readFileSync('./yarn.lock').toString();
+    const yarnlock = YarnLock.parse(yarnlockfile);
 
-const urls = new Set;
-for (let name in yarnlock.object) {
-    const url = yarnlock.object[name].resolved;
+    const urls = new Set;
+    for (let name in yarnlock.object) {
+        const url = yarnlock.object[name].resolved;
 
-    const parsed = Url.parse(url);
-    let basename = path.basename(parsed.pathname);
-    if (name.startsWith('@')) {
-        const namespace = name.substring(0, name.indexOf('/'));
-        basename = namespace + '-' + basename;
+        const parsed = Url.parse(url);
+        let basename = path.basename(parsed.pathname);
+        if (name.startsWith('@')) {
+            const namespace = name.substring(0, name.indexOf('/'));
+            basename = namespace + '-' + basename;
+        }
+
+        urls.add([url, basename]);
     }
 
-    urls.add([url, basename]);
+    const sources = [];
+    for (let [url, basename] of urls) {
+
+        const sha256 = crypto.createHash('sha256');
+        const [buffer,] = await Tp.Helpers.Http.get(url, { raw: true });
+        sha256.update(buffer);
+
+        const source = {
+            type: 'file',
+            url: url,
+            sha256: sha256.digest('hex'),
+            dest: 'deps',
+            'dest-filename': basename
+        };
+        sources.push(source);
+    }
+
+    fs.writeFileSync('./build-data/yarn.json', JSON.stringify(sources, undefined, 4));
 }
-
-const sources = [];
-for (let [url, basename] of urls) {
-
-    const sha256 = crypto.createHash('sha256');
-    sha256.update(fs.readFileSync(path.resolve('deps', basename)));
-
-    const source = {
-        type: 'file',
-        url: url,
-        sha256: sha256.digest('hex'),
-        dest: 'deps',
-        'dest-filename': basename
-    };
-    sources.push(source);
-}
-
-fs.writeFileSync('./build-data/yarn.json', JSON.stringify(sources, undefined, 4));
+main();
