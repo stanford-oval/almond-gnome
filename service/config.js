a9b4e90b60063339d6e50bdc0c1290cae077a9fd
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-30 10:38:43 -0700

    Restore production almond NLP server
    To test some of the skills, I need working natural language

--- service/config.js
+++ service/config.js
@@ -9,7 +9,7 @@
 // See COPYING for details
 "use strict";
 
-module.exports.SEMPRE_URL = 'https://nlp-staging.almond.stanford.edu';
+module.exports.SEMPRE_URL = 'https://almond-nl.stanford.edu';
 module.exports.THINGPEDIA_URL = 'https://almond-dev.stanford.edu/thingpedia';
 module.exports.MS_SPEECH_RECOGNITION_PRIMARY_KEY = 'de1f02817356494483ba502b2ce95f6f';
 module.exports.MS_SPEECH_RECOGNITION_SECONDARY_KEY = '3dc6ce0b832940f0b0c984a1517c457e';
