21cdb03c94cace96d1398f43bdb40c41ff9208c2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-08-02 22:41:05 -0700

    Relicense to Apache 2.0
    For consistency with everything else in Genie/Almond

--- src/app/main.js
+++ src/app/main.js
@@ -1,8 +1,21 @@
-// -*- Mode: js; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*-
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
 //
-// Copyright 2016 Giovanni Campagna <gcampagn@cs.stanford.edu>
+// This file is part of Almond
 //
-// See LICENSE for details
+// Copyright 2016-2019 The Board of Trustees of the Leland Stanford Junior University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 "use strict";
 
 pkg.initGettext();
